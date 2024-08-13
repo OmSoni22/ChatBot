2245f9dd952b3cff5c9f62f7ef7831ef15bd82cc
OmSoni22 <soniom2002.os@gmail.com>
2024-08-13 21:55:39 +0530

    Remote Ollama added

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -5,7 +5,7 @@ from apps.home import blueprint
 from apps.llm.llm_provider import ChatLLM
 from apps.models import Chats
 
-chat = ChatLLM(model='llama3')
+chat = ChatLLM()
 
 
 @blueprint.route('/')
@@ -13,4 +13,4 @@ def index():
     # chat_record = Chats(title='Om')
     # db.session.add(chat_record)
     # db.session.commit()
-    return chat.get_title('National Bird', 'Peacock'), 201
+    return chat.get_response('National Bird of India?'), 201
--- apps/llm/llm_provider.py
+++ apps/llm/llm_provider.py
@@ -1,12 +1,13 @@
 import ollama
 
+client = ollama.Client(host='https://d335-34-16-195-102.ngrok-free.app')
 
 class ChatLLM:
     def __init__(self, model='llama3.1'):
         self.model = model
 
     def get_title(self, prompt, response):
-        response = ollama.chat(model=self.model, messages=[
+        response = client.chat(model=self.model, messages=[
             {
                 'role': 'user',
                 'content': f'return a 2 to 4 words title for this user prompt:{prompt} and ai response: {response}. You must '
@@ -16,7 +17,7 @@ class ChatLLM:
         return response['message']['content']
 
     def get_response(self, prompt):
-        response = ollama.chat(model=self.model, messages=[
+        response = client.chat(model=self.model, messages=[
           {
             'role': 'user',
             'content': f'{prompt}',
